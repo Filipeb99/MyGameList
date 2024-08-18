86c1745f001918974fa86d380f6b3bde509436b4
Filipe Borralho <filipeborralho99@gmail.com>
2024-08-18 16:34:37 +0200

    Enrich UI with add and save buttons

--- MainUI.java
+++ MainUI.java
@@ -1,19 +1,33 @@
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+import javax.swing.JButton;
 
 public class MainUI {
     private JFrame window = null;
     private JTable table = null;
-    private JScrollPane container = null;
+    private JScrollPane scrollPane = null;
+    private JToolBar toolbar = null;
+    private JButton addButton = null;
+    private JButton saveButton = null;
     
     public MainUI(Object[][] data, String[] colNames, String title) {
         window = new JFrame(title);
         
         table = new JTable(data, colNames);
-        container = new JScrollPane(table);
+        scrollPane = new JScrollPane(table);
         
-        window.add(container);
+        toolbar = new JToolBar();
+        addButton = new JButton("Add");
+        saveButton = new JButton("Save");
+        
+        toolbar.add(addButton);
+        toolbar.add(saveButton);
+        
+        window.add(scrollPane, BorderLayout.CENTER);
+        window.add(toolbar, BorderLayout.SOUTH);
         
         window.pack();
         window.setResizable(true);
